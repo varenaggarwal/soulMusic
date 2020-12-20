84d5cba7fe94b28287ad28d55fe8a40003a2212f
Varen Aggarwal <varenaggarwal@gmail.com>
2020-12-20 17:17:13 +0530

    add dictionary, display dictionary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
-
+let soulMusicDictionary = {
+  Rock: [
+    { name: "Under Pressure by Queen & David Bowie", rating: "5/5" },
+    { name: "Comfortably Numb by Pink Floyd", rating: "4.5/5" }
+  ],
+  Pop: [
+    { name: "Billie Jean by Micheal Jackson", rating: "5/5" },
+    { name: "Everybody by Backstreet Boys", rating: "4.5/5" }
+  ],
+  HipHop: [
+    { name: "Rapper's Delight by Sugarhill Gang", rating: "5/5" },
+    { name: "Mind Playing Tricks on Me by Gheto Boys", rating: "4.5/5" }
+  ]
+};
+let songList = [];
+let genresList = Object.keys(soulMusicDictionary);
 export default function App() {
+  const [currentgenre, setCurrentgenre] = useState("Rock");
+  function onClickHandler() {
+    // setCurrentgenre()
+  }
   return (
     <div className="App">
-      <h1>Soul Music</h1>
+      <h1>Soul Music 🎼</h1>
+      <div>Checkout recommendations as per genres</div>
+      {genresList.map((genre) => (
+        <button onClick={onClickHandler}>{genre}</button>
+      ))}
+      <hr />
+      {soulMusicDictionary[currentgenre].map((song) => (
+        <li>
+          <div>{song.name}</div>
+          <div>{song.rating}</div>
+        </li>
+      ))}
     </div>
   );
 }
